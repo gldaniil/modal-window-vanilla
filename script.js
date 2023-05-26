e14f4eec19da0ff97fa97063a9d930d6bb33b65c
gldaniil <daniil.gajfullin@yandex.ru>
2023-05-27 02:39:11 +0500

    Кнопка + стили

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-const panelTitle = document.querySelector(".panel-base__title");
-const panelTitleText = "Привет! Это модальное окно! Оно появится на следующем листе.";
-const speed = 150;
+const panelTitle = document.querySelector(".panel-base__text");
+const panelTitleText =
+  "Привет! Это модальное окно!<new>Возможность вызвать его скоро появится :)";
+const splitted = panelTitleText.split("<new>");
+
+splitted.forEach(() => {
+  const p = document.createElement("p");
+  panelTitle.append(p);
+});
+
+const paras = panelTitle.querySelectorAll("p");
 let i = 0;
+let currentText = 0;
 
-const typeWriter = () => {
-  const cursor = panelTitle.nextElementSibling;
-  cursor.classList.add("typing");
-  if (i < panelTitleText.length) {
-    panelTitle.innerHTML += panelTitleText.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  } else cursor.classList.remove("typing");
+function gradualAppearence() {
+  return new Promise((resolve) => {
+    const intervalId = setInterval(() => {
+      paras[currentText].innerHTML += splitted[currentText][i];
+      i++;
+
+      if (i === splitted[currentText].length) {
+        currentText++;
+        i = 0;
+
+        if (currentText === splitted.length) {
+          clearInterval(intervalId);
+          resolve();
+        }
+      }
+    }, 75);
+  });
+}
+gradualAppearence().then(() => {
+  boby();
+});
+const boby = () => {
+  const buttonOpenModal = document.createElement("button");
+  buttonOpenModal.innerText = "Открыть"
+  buttonOpenModal.className = "panel-base__button"
+  console.log(panelTitle.parentElement.appendChild(buttonOpenModal))
 };
-
-typeWriter();
