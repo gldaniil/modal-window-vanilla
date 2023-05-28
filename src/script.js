3c1a679e08f87027bbe70db11ec49e61108dcc28
gldaniil <daniil.gajfullin@yandex.ru>
2023-05-28 23:18:55 +0500

    Оптимизация кода + комментарии

--- src/script.js
+++ src/script.js
@@ -1,53 +1,59 @@
-const panelTitle = document.querySelector(".panel-base__text");
-const panelTitleText =
+const panel = document.querySelector(".panel-base__text");
+const panelText =
   "Привет! Это модальное окно!<new>Возможность вызвать его скоро появится :)";
-const splitted = panelTitleText.split("<new>");
 
-splitted.forEach(() => {
-  const p = document.createElement("p");
-  panelTitle.append(p);
-});
+const gradualAppearence = () => {
+  // Массива фраз из предложения по разделителю <new>
+  const phrases = panelText.split("<new>");
+  // Создание <p> для каждой фразы из массива
+  phrases.forEach(() => {
+    const p = document.createElement("p");
+    panel.append(p);
+  });
 
-const paras = panelTitle.querySelectorAll("p");
-let i = 0;
-let currentText = 0;
-
-function gradualAppearence() {
+  let i = 0, currentText = 0;
+  const panelStrings = panel.querySelectorAll("p");
+  // Промис для появления кнопки строго после напечатанного текста
   return new Promise((resolve) => {
-    const intervalId = setInterval(() => {
-      paras[currentText].innerHTML += splitted[currentText][i];
+    const printedText = setInterval(() => {
+      // Назначение класса для эффекта "печатающегося текста"
+      if (!panelStrings[currentText].classList.contains("typing"))
+        panelStrings[currentText].classList.add("typing");
+      // Добавление по 1 символу
+      panelStrings[currentText].innerHTML += phrases[currentText][i];
       i++;
-
-      if (i === splitted[currentText].length) {
+      // Если строка полностью напечатана - убираем эффект
+      if (i === phrases[currentText].length) {
+        panelStrings[currentText].removeAttribute("class");
         currentText++;
         i = 0;
-
-        if (currentText === splitted.length) {
-          clearInterval(intervalId);
+        /* Если все фразы из массива добавлены в документ, то
+          то отменяем установленный интервал printedText. */
+        if (currentText === phrases.length) {
+          clearInterval(printedText);
           resolve();
         }
       }
       // 75
-    }, 0);
+    }, 100);
   });
-}
-gradualAppearence().then(() => {
-  boby();
-});
-const boby = () => {
-  const buttonOpenModal = document.createElement("button");
-  buttonOpenModal.dataset.func = "open";
-  buttonOpenModal.innerText = "Открыть";
-  buttonOpenModal.className = "panel-base__button";
-  panelTitle.parentElement.appendChild(buttonOpenModal);
 };
+/* Вызов функции "печатающегося текста" и ожидание результата выполнения. 
+  После выполнения добавляется кнопка открытия модального окна. */
+gradualAppearence().then(() => {
+  const openModal = document.createElement("button");
+  openModal.dataset.func = "open";
+  openModal.innerText = "Открыть";
+  openModal.className = "panel-base__button";
+  panel.parentElement.appendChild(openModal);
+});
 
 const closeModal = (e) => {
   if (e.key === "Escape") {
     modal.classList.remove("open");
     document.removeEventListener("keydown", closeModal);
   }
-}
+};
 
 const pageClickHandling = (e) => {
   const target = e.target;
@@ -59,16 +65,17 @@ const pageClickHandling = (e) => {
 document.body.addEventListener("click", (e) => pageClickHandling(e));
 
 // Modal Window
+const modal = document.querySelector(".modal");
 const modalClickHandling = (e) => {
   const target = e.target;
+  // Если клик вне модального (заблюренная область) или по крестику
   if (target.classList.contains("modal") || target.dataset.func === "close") {
     modal.classList.remove("open");
   }
   console.log(target);
   if (target.dataset.func === "send") {
-    console.log("Continue...")
+    console.log("Continue...");
   }
   e.preventDefault();
 };
-const modal = document.querySelector(".modal");
 modal.addEventListener("click", (e) => modalClickHandling(e));
